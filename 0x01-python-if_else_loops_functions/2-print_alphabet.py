a33ad4c328cf5b8c31bb30fa128f890f60d71001
gonjetso <gonjetsoc@gmail.com>
2022-11-29 12:22:54 -0800

    Task 2

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-"""Print the alphabet in lowercase"""
 
-for i in range(97,123):
-     print("{:c}".format(i), end='')
+for i in range(97, 123):
+    print("{}".format(chr(i)), end="")
