3a23011b3bf610b7d2e3e2b0944c5c9e540d3d6f
gonjetso <gonjetsoc@gmail.com>
2022-12-02 07:35:59 -0800

    Removed , at the end of 99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -2,4 +2,7 @@
 
 """Print 0 to 99"""
 for i in range(0, 100):
-    print("{:02}, ".format(i), end=", ")
+    if i == 99:
+        print("{}".format(i))
+    else:
+        print("{:02}, ".format(i), end=", ")
