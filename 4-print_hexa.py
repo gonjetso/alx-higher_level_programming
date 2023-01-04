80b37028b79e1c6471510081df848ed473aacdd5
gonjetso <gonjetsoc@gmail.com>
2023-01-04 10:34:04 -0800

    Formatting

--- 4-print_hexa.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-
-"""Print 0 to 98 in hexadecimal"""
-for i in range(0, 99):
-    print("{} = {}".format(i, hex(i)))
--- 5-print_comb2.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-
-"""Print 0 to 99"""
-for i in range(0, 100):
-    print("{:02}, ".format(i), end=", ")
