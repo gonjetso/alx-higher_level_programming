40f84690ef263de617ae9974d743c87bd5e14c69
gonjetso <gonjetsoc@gmail.com>
2023-01-12 16:25:01 +0200

    Update Task 14

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 # Author - Gonjetso
 
-for i in range(122, 96, -1):
-    if i % 2 == 0:
-        print(chr(i), end='')
-    else:
-        print(chr(i-32), end='')
+i = 0
+for c in range(ord('z'), ord('a') - 1, -1):
+    print("{}".format(chr(c - i)), end="")
+    i = 32 if i == 0 else 0
