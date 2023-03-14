9eb35a1483f0f4490625b325dc63237cd54a6fbc
gonjetso <gonjetsoc@gmail.com>
2023-03-14 23:09:51 +0200

    Update 2-rectangle.js

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,9 +1,11 @@
 #!/usr/bin/node
 class Rectangle {
   constructor(w, h) {
-    if (w > 0 && h > 0) {
+    if (w > 0 && h > 0 && Number.isInteger(w) && Number.isInteger(h)) {
       this.width = w;
       this.height = h;
+    } else {
+      return {};
     }
   }
 }
