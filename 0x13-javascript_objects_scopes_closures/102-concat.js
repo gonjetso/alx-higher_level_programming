32cceec36336ebc60c8709f56bb8c8d6e6c54ccf
gonjetso <gonjetsoc@gmail.com>
2023-03-14 23:15:39 +0200

    Update 102-concat.js

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,6 +1,14 @@
 #!/usr/bin/node
-const ab = require('ab');
+const fs = require('fs');
 
-const aArg = ab.readFileSync(process.argv[2]).toString();
-const bArg = ab.readFileSync(process.argv[3]).toString();
-ab.writeFileSync(process.argv[4], aArg + bArg);
+const sourceFile1 = process.argv[2];
+const sourceFile2 = process.argv[3];
+const destFile = process.argv[4];
+
+const file1Contents = fs.readFileSync(sourceFile1);
+const file2Contents = fs.readFileSync(sourceFile2);
+
+const result = Buffer.concat([file1Contents, file2Contents]);
+
+fs.writeFileSync(destFile, result);
+console.log(`Successfully concatenated ${sourceFile1} and ${sourceFile2} to ${destFile}.`);
